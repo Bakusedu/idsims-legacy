7b5a05ec68b4bb970fe3efc182bae8162138d32a
Chiedu Emmanuel <iamnedu912@gmail.com>
2023-02-01 06:53:30 +0100

    Fix cors test on forge

--- public/index.php
+++ public/index.php
@@ -44,6 +44,10 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
