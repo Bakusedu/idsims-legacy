858b358d502c05a17b4f9332bec66c7f185061b7
Chiedu Emmanuel <iamnedu912@gmail.com>
2023-02-01 07:18:09 +0100

    Reverted cors middleware

--- public/index.php
+++ public/index.php
@@ -44,9 +44,9 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+// header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
